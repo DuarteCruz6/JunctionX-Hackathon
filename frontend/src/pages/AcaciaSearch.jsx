abcaa6810fc3f0135d9c58ad5473e958775a0886
André Pagaime <andre.pagaime2014@gmail.com>
2025-10-18 21:27:23 +0100

    Msgs erro

--- frontend/src/pages/AcaciaSearch.jsx
+++ frontend/src/pages/AcaciaSearch.jsx
@@ -149,6 +149,8 @@ const AcaciaSearch = () => {
         loginForm={auth.loginForm}
         onFormChange={auth.handleLoginFormChange}
         onSubmit={auth.handleLoginSubmit}
+        isLoading={auth.isLoading}
+        error={auth.error}
       />
 
       <ReportsModal
